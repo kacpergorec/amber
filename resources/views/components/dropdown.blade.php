b14c629d7c4db99f593d8169a2c9f60ebb7c82be
kacpergorec <kacpergorec@gmail.com>
2024-11-12 21:16:42 +0100

    fix: accessibility focus on menu

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -17,21 +17,30 @@
         default => $width,
     };
 @endphp
-
-<div class="relative whitespace-nowrap inline-flex items-center" x-data="{ open: false }" @click.outside="open = false"
-    @close.stop="open = false" @mouseover="if ({{ $hoverable ?: 'false' }}) open = true"
-    @mouseleave="if ({{ $hoverable ?: 'false' }}) open = false">
-    <div @click="open = ! open" class="h-full flex">
+<div class="relative whitespace-nowrap inline-flex items-center one"
+     x-data="{ open: false }"
+     @click.outside="open = false"
+     @close.stop="open = false"
+     @mouseover="if ({{ $hoverable ?: 'false' }}) open = true"
+     @mouseleave="if ({{ $hoverable ?: 'false' }}) open = false"
+     @focus="open = true"
+     tabindex="0"
+>
+    <div @click="open = ! open" class="h-full flex four">
         {{ $trigger }}
     </div>
 
-    <div x-show="open" x-transition:enter="transition ease-out duration-200"
-        x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100"
-        x-transition:leave="transition ease-in duration-75" x-transition:leave-start="opacity-100 scale-100"
-        x-transition:leave-end="opacity-0 scale-95"
-        class="absolute z-50 pt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }} top-full"
-        style="display: none;" @click="open = false">
-        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
+    <div x-show="open"
+         x-transition:enter="transition ease-out duration-200"
+         x-transition:enter-start="opacity-0 scale-95"
+         x-transition:enter-end="opacity-100 scale-100"
+         x-transition:leave="transition ease-in duration-75"
+         x-transition:leave-start="opacity-100 scale-100"
+         x-transition:leave-end="opacity-0 scale-95"
+         @click="open = false"
+         class="absolute z-50 pt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }} top-full two"
+         style="display: none;">
+        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }} three">
             {{ $content }}
         </div>
     </div>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,8 +3,8 @@
 @php
     $classes =
         $active ?? false
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-primary-400 dark:border-primary-600 text-sm font-medium leading-5 text-zinc-900 dark:text-zinc-100 focus:outline-none focus:border-primary-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-zinc-500 dark:text-zinc-400 hover:text-zinc-700 dark:hover:text-zinc-300 hover:border-zinc-300 dark:hover:border-zinc-700 focus:outline-none focus:text-zinc-700 dark:focus:text-zinc-300 focus:border-zinc-300 dark:focus:border-zinc-700 transition duration-150 ease-in-out';
+            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-primary-400 dark:border-primary-600 text-sm font-medium leading-5 text-zinc-900 dark:text-zinc-100 transition duration-150 ease-in-out'
+            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-zinc-500 dark:text-zinc-400 hover:text-zinc-700 dark:hover:text-zinc-300 hover:border-zinc-300 dark:hover:border-zinc-70 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
